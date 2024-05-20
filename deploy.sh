345973ba59f4425e18fa54dbe05053c408796f69
justin kao <justinkao@justindeMacBook-Pro.local>
2024-05-20 17:02:35 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,37 @@
 
 cd /home/ubuntu/app
 
-echo Stopping any existing containers...
-docker-compose down
+# Check if the postgres container is running
+if docker ps | grep -q postgres; then
+  echo "Postgres container is already running."
+else
+  echo "Postgres container is not running. Starting docker-compose services..."
+  docker-compose up -d
+fi
 
-echo Pulling the latest images...
-docker-compose pull
-
-echo Starting the application...
-docker-compose up -d
-
+container_id_frontend=$(docker ps -q --filter ancestor=hoppscotch/hoppscotch-frontend)
+if [ -n "$container_id_frontend" ]; then
+  echo "Container with image hoppscotch/hoppscotch-frontend is running. Stopping the container..."
+  docker stop "$container_id_frontend"
+else
+  echo "No running container with image hoppscotch/hoppscotch-frontend found."
+fi
 docker run -d -p 3000:3000 --env-file .env --network app_hoppscotch-network --restart unless-stopped hoppscotch/hoppscotch-frontend
+
+container_id_backend=$(docker ps -q --filter ancestor=hoppscotch/hoppscotch-backend)
+if [ -n "$container_id_backend" ]; then
+  echo "Container with image hoppscotch/hoppscotch-backend is running. Stopping the container..."
+  docker stop "$container_id_backend"
+else
+  echo "No running container with image hoppscotch/hoppscotch-backend found."
+fi
 docker run -d -p 3170:3170 --env-file .env --network app_hoppscotch-network --restart unless-stopped hoppscotch/hoppscotch-backend
+
+container_id_admin=$(docker ps -q --filter ancestor=hoppscotch/hoppscotch-admin)
+if [ -n "$container_id_admin" ]; then
+  echo "Container with image hoppscotch/hoppscotch-admin is running. Stopping the container..."
+  docker stop "$container_id_admin"
+else
+  echo "No running container with image hoppscotch/hoppscotch-frontend found."
+fi
 docker run -d -p 3100:3100 --env-file .env --network app_hoppscotch-network --restart unless-stopped hoppscotch/hoppscotch-admin
